9fca6f1d73db1371ec0ba2189989b03240525238
kntjrld <kentjarold57@gmail.com>
2023-02-14 10:12:25 +0800

    commit

--- src/Home.java
+++ src/Home.java
@@ -1,7 +1,14 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Home extends JFrame{
     private JPanel MainJPanel;
+    private JButton BtnDashboard;
+    private JButton BtnLogout;
+    private JButton reportsButton;
+    private JButton recordsButton;
+    private JButton settingsButton;
 
     public static void main(String[] args) {
         Home home = new Home();
@@ -10,5 +17,16 @@ public class Home extends JFrame{
         super();
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(MainJPanel);
+        BtnLogout.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                main m =  new main();
+                m.pack();
+                m.setVisible(true);
+                m.setResizable(false);
+                m.setLocationRelativeTo(null);
+            }
+        });
     }
 }
